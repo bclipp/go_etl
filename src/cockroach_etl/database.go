cb3608d0aec3a052491bd61d1d43bafcd35b5004
blipp <bclipp770@gmail.com>
2020-07-30 19:21:14 -0400

    adding basic ETL go wrapper

---
+++ src/cockroach_etl/database.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Database interface {
+	connect()  error
+	Disconnect()
+	sendQueryReturnData(sqlQuery string)(*sql.Rows, error)
+	sendQuery(query string)  (sql.Result, error)
+}
+
+type CockroachDB struct {
+	DB               *sql.DB
+	IPAddress        string
+	PostgresDB       string
+}
+
+func (cr *CockroachDB) Connect() error {
+	psqlInfo := fmt.Sprintf("host=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cr.IPAddress, cr.PostgresUser, cr.PostgresPassword, cr.PostgresDB)
+
+	db, err := sql.Open("postgres", psqlInfo);if err != nil {
+		return err
+	}
+	cr.DB = db
+
+	err = cr.DB.Ping();if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cr *CockroachDB) Disconnect() {
+	_ = cr.DB.Close()
+}
+
+func (cr *CockroachDB) sendQueryReturnData(sqlQuery string)(*sql.Rows, error) {
+	rows, err := cr.DB.Query(sqlQuery)
+	return rows, err
+}
+
+func (cr *CockroachDB) SendQuery(query string) (sql.Result, error) {
+	result, err := cr.DB.Exec(query)
+	if err != nil { return result, err}
+
+	return result, nil
+}
--- src/cockroach_etl/main.go
+++ src/cockroach_etl/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	var cr = CockroachDB{
+		IPAddress:  "localhost",
+		PostgresDB: "test",
+	}
+	err := cr.Connect();if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer cr.Disconnect()
+
+}
